91c5d2076b326fa24a2fde2b028bd0509ecbe702
Ryan Crowder <crowderyan@gmail.com>
2021-06-07 15:53:25 -0400

    the functional Marvel API model

---
+++ assets/js/marvel-api-client.js
@@ -0,0 +1,35 @@
+
+// you will also have to setup the referring domains on your marvel developer portal
+var PRIV_KEY = "this-should-be-a-long-hash";
+var PUBLIC_KEY = "so-should-this";
+
+function getMarvelResponse() {
+
+  // you need a new ts every request                                                                                    
+  var ts = new Date().getTime();
+  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  
+  // the api deals a lot in ids rather than just the strings you want to use
+  var characterId = '1009718'; // wolverine                                                                             
+
+
+  var url = 'http://gateway.marvel.com:80/v1/public/comics';
+
+  console.log(url);
+  $.getJSON(url, {
+    ts: ts,
+    apikey: PUBLIC_KEY,
+    hash: hash,
+    characters: characterId
+    })
+    .done(function(data) {
+      // sort of a long dump you will need to sort through
+      console.log(data);
+    })
+    .fail(function(err){
+      // the error codes are listed on the dev site
+      console.log(err);
+    });
+};
+
+getMarvelResponse();
